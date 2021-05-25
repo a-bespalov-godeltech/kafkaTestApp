580f34f50edd4ec8495a244b8d1c259e3ae163f4
Aleksandr Bespalov <a.bespalov@godeltech.com>
2021-05-25 17:41:00 +0300

    Init

---
+++ src/main/java/com/test/KafkaTestApp/KafkaTestAppApplication.java
@@ -0,0 +1,12 @@
+package com.test.KafkaTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaTestAppApplication.class, args);
+  }
+}
