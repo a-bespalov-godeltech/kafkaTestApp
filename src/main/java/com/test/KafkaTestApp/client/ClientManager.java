393dcad3276661a3cf5f5ef4a3a6a4c71b73971e
Aleksandr Bespalov <a.bespalov@godeltech.com>
2021-05-28 18:13:05 +0300

    Kafka test added

--- src/main/java/com/test/KafkaTestApp/client/ClientManager.java
+++ src/main/java/com/test/KafkaTestApp/client/ClientManager.java
@@ -1,24 +1,32 @@
 package com.test.KafkaTestApp.client;
 
 import com.test.KafkaTestApp.config.KafkaConfig;
-import lombok.AllArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ClientManager {
 
   private final KafkaTemplate<String, Client> kafkaTemplate;
   private final KafkaConfig kafkaConfig;
+  private List<Client> clients = new ArrayList<>();
 
   public void create(Client client) {
     kafkaTemplate.send(kafkaConfig.getClientTopic(), client);
   }
 
-  @KafkaListener(id = "KafkaListenerExample", topics = {"#{'${spring.kafka.client-topic}'}"})
+  @KafkaListener(id = "KafkaListenerExample", topics = "${spring.kafka.client-topic}")
   public void consume(Client client) {
     System.out.println(client.getClientId() + " " + client.getEmail());
+    clients.add(client);
+  }
+
+  public List<Client> getClients() {
+    return clients;
   }
 }
